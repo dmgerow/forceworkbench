465d02ec276d7e46c7ecbd875b7e7ab4b7ef0525
Dylan Gerow <dgerow@wmp.com>
2019-08-19 18:31:59 -0500

    try

--- workbench/config/overrides.php
+++ workbench/config/overrides.php
@@ -43,6 +43,7 @@ It is recommended that you change the values below to custom strings.
  */
 $config["sodiumKey"]["default"] = "T8TAoGtlCWOwWrFFRjTThFDn9+iGsGGjhvALbWkSONN4KyDNbI2VNZmm+sCiM5X7";
 $config["nonce"]["default"] = "aojzmL4AKy1s5T5JnQ1yn+2U";
+$config["loginCsrfEnabled"]["default"]=false;
 
 // ORG ID WHITELIST / BLACKLIST
 // To only allow access to a set of orgs or block access to particular orgs,
